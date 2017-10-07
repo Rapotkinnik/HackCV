6a6501b19d026bc6e294253f8a2f6358fe5a7346
Anton Myagkov <myagkov-a@yandex.ru>
2017-10-07 14:28:11 +0300

    show some picture

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
-int main(int argc, char *argv[])
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+using namespace std;
+using namespace cv;
+
+char window_name[] = "Demo";
+
+int main(void)
 {
-    return 0;
+	namedWindow(window_name, WINDOW_AUTOSIZE);
+	auto src = imread("D:\\test\\1.jpg", IMREAD_COLOR);
+    
+	imshow(window_name, src);
+	waitKey(0);
+	return 0;
 }

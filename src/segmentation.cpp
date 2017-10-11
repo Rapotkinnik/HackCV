6d326ccee7f634b0a712f8250ffdbf3afdbe2798
antmya <antmya@transas.com>
2017-10-11 10:34:23 +0300

    fix

--- src/segmentation.cpp
+++ src/segmentation.cpp
@@ -1,10 +1,8 @@
 #include "segmentation.h"
 
-cv::Mat1b PickOutRedColor(cv::Mat3b image) {
-  cv::Mat3b bgr = cv::imread("path_to_image");
-
+cv::Mat1b PickOutRedColor(cv::Mat image) {
   cv::Mat3b hsv;
-  cvtColor(bgr, hsv, cv::COLOR_BGR2HSV);
+  cvtColor(image, hsv, cv::COLOR_BGR2HSV);
 
   cv::Mat1b mask1, mask2;
   cv::inRange(hsv, cv::Scalar(0, 70, 50), cv::Scalar(10, 255, 255), mask1);
@@ -15,14 +13,14 @@ cv::Mat1b PickOutRedColor(cv::Mat3b image) {
 
 
 
-std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat1b image)
+std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat image)
 {
-  cv::medianBlur(image, image, 5);
+  cv::medianBlur(image, image, 1);
   std::vector<cv::Vec3f> circles;
 
   cv::HoughCircles(image, circles, cv::HOUGH_GRADIENT, 1,
                    image.rows/16,  // change this value to detect circles with different distances to each other
-               100, 30, 1, 30 // change the last two parameters
+               100, 20, 10, 60 // change the last two parameters
     // (min_radius & max_radius) to detect larger circles
   );
 
--- src/segmentation.h
+++ src/segmentation.h
@@ -4,7 +4,7 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
-cv::Mat1b PickOutRedColor(cv::Mat3b image);
+cv::Mat1b PickOutRedColor(cv::Mat image);
 
 
-std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat1b image);
+std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat image);

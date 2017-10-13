c112dc16e7b322ea78e2731060f0062bd27ea01a
Marsel Galimullin <mfgalimullin@yandex.ru>
2017-10-13 20:39:06 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 mkdir build
 cd build
+conan remote add piponazo https://api.bintray.com/conan/piponazo/piponazo
 conan install --build=missing  -s compiler.libcxx=libstdc++ ..
 cmake ..
 cmake --build . -- -j$(nproc --all)

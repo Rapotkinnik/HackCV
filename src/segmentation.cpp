8874e7f63296b94a57932ed5175634449e386297
Myagkov <antmya@transas.com>
2017-10-15 01:00:20 +0300

    find contour for road sign

--- src/segmentation.cpp
+++ src/segmentation.cpp
@@ -11,6 +11,25 @@ cv::Mat1b PickOutRedColor(cv::Mat image) {
   return mask1 | mask2;
 }
 
+cv::Mat1b PickOutWhiteColor(cv::Mat image) {
+	cv::Mat3b hsv;
+	cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+
+	cv::Mat1b mask1, mask2;
+
+	cv::inRange(hsv, cv::Scalar(0, 0, 150, 0), cv::Scalar(180, 255, 255, 0), mask1);
+	return mask1;
+}
+
+
+cv::Mat1b PickOutBlueColor(cv::Mat image) {
+	cv::Mat3b hsv;
+	cvtColor(image, hsv, cv::COLOR_BGR2HSV);
+
+	cv::Mat1b mask1;
+	cv::inRange(hsv, cv::Scalar(100, 70, 50), cv::Scalar(130, 255, 255), mask1);
+	return mask1;
+}
 
 
 std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat image)
@@ -25,4 +44,20 @@ std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat image)
   );
 
   return circles;
+}
+
+std::vector<std::vector<cv::Point>> findContours(cv::Mat image)
+{
+	int erosion_size = 6;
+	cv::Mat element = getStructuringElement(cv::MORPH_ELLIPSE,
+		cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+		cv::Point(erosion_size, erosion_size));
+
+	cv::Mat dilate_img;
+	cv::dilate(image, dilate_img, element);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(dilate_img, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+	return contours;
 }
--- src/segmentation.h
+++ src/segmentation.h
@@ -5,6 +5,9 @@
 #include "opencv2/highgui.hpp"
 
 cv::Mat1b PickOutRedColor(cv::Mat image);
+cv::Mat1b PickOutWhiteColor(cv::Mat image);
+cv::Mat1b PickOutBlueColor(cv::Mat image);
 
 
 std::vector<cv::Vec3f> findCircleRoadSigns(cv::Mat image);
+std::vector<std::vector<cv::Point>> findContours(cv::Mat image);

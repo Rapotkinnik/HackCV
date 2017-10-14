66a0a563cdc4d56bbdf8fa8722711e0ac64b7403
Marsel Galimullin <mfgalimullin@yandex.ru>
2017-10-14 13:39:22 +0300

    добавил поддержку travis-ci

---
+++ travis/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    if which pyenv > /dev/null; then
+        eval "$(pyenv init -)"
+    fi
+    pyenv activate conan
+fi
+
+export CXX=$CXX_COMPILER
+export CC=$C_COMPILER
+
+mkdir _build && cd _build
+conan install --build=missing  -s compiler.libcxx=libstdc++ ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER ..
+cmake --build . -- -j$(nproc --all)
